7c986b51d2cd63f40951c01e2046ad4cc00baf86
joshuacassidygrant <jc.grant22@gmail.com>
2019-06-04 17:37:15 -0700

    basic draft navbar styles

--- imports/ui/components/AppShell.jsx
+++ imports/ui/components/AppShell.jsx
@@ -6,16 +6,16 @@ import {Link} from 'react-router-dom';
 class AppShell extends Component {
   render () {
     return (
-      <div>
+      <div className="app-shell">
         <nav>
-          //Temporary. Replace with a navbar component etc.
-          I'm a navbar.
           <Link to='/'>Home </Link>
           <Link to='/tp2/123'>123 </Link>
           <hr/>
         </nav>
-        // Renders any children of this node.
-        {this.props.children}
+        <main>
+          // Renders any children of this node.
+          {this.props.children}
+        </main>
         <footer>
           //Temporary. Replace witj a footer component etc.
           <hr/>
