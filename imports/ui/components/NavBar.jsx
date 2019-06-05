1580c27e9e7a0075109c15e274607cada1aafb26
joshuacassidygrant <jc.grant22@gmail.com>
2019-06-04 18:37:19 -0700

    Added icons in menu

--- imports/ui/components/NavBar.jsx
+++ imports/ui/components/NavBar.jsx
@@ -1,19 +1,28 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import NotificationsIndicator from './NotificationsIndicator';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSeedling, faSearch, faHiking, faBell } from '@fortawesome/free-solid-svg-icons';
 
 export default class NavBar extends Component {
 
   render() {
     return (
       <nav>
-        <Link to='/'>Gardens</Link>
-        <Link to='/search'>Database</Link>
-        <Link to='/tp2/123'>Profile</Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faSeedling} />
+        </Link>
+        <Link to='/search'>
+          <FontAwesomeIcon icon={faSearch} />
+        </Link>
+        <Link to='/tp2/123'>
+          <FontAwesomeIcon icon={faHiking} />
+        </Link>
         <hr/>
         <div className="notifications-holder">
-          <Link to='/tp2/123'>Notifications
+          <Link to='/tp2/123'>
             <NotificationsIndicator count="2" />
+            <FontAwesomeIcon icon={faBell} />
           </Link>
         </div>
       </nav>
