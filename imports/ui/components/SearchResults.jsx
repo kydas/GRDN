6cf13df91832a44c4135019f502342b3e56200ea
joshuacassidygrant <jc.grant22@gmail.com>
2019-06-18 18:26:21 -0700

    Search form simple (no displays)

---
+++ imports/ui/components/SearchResults.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class SearchResults extends Component {
+
+
+  render() {
+    console.log(this.props.results);
+    return (
+      <div>
+        PUT RESULTS HERE
+      </div>
+    )
+
+  }
+}
