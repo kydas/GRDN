55add35c944ed2127553ba68e732ce68230fa16e
dasilvar93 <kadasilva22@gmail.com>
2019-07-02 18:36:44 -0700

    moved trefleAPI  and apiMethods to plants folder

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import Links from '/imports/api/links';
-import methodsTest from './apiMethods'
+import methodsTest from './Plants/apiMethods'
 import userMethods from '/imports/api/userMethods';
 import './Gardens/GardenMethods';
 
