5b34a95e0f86f9a7f41014261deb70c9f3949286
joshuacassidygrant <jc.grant22@gmail.com>
2019-08-05 08:58:54 -0700

    fixed imports for local

--- imports/ui/pages/GardenPage.jsx
+++ imports/ui/pages/GardenPage.jsx
@@ -3,6 +3,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import PlantList from '../components/PlantList';
 import fetchGardenById from '../actions/GardenActions';
 import MapContainer from '../components/MapContainer';
+import WeatherWidget from '../components/WeatherWidget';
 
 export default class GardenPage extends Component{
 
@@ -32,10 +33,13 @@ export default class GardenPage extends Component{
           <div className="row">
             <h1>Garden: {this.state.entry.name}</h1>
             <div className="col two-third">
-            <PlantList gardenId={this.state.entry._id} plants={this.state.entry.plants} />
+              <h2>Plants</h2>
+              <PlantList gardenId={this.state.entry._id} plants={this.state.entry.plants} />
             </div>
             <div className="col third">
-            <MapContainer center={
+              <WeatherWidget garden={this.state.entry} />
+              <h2>Location</h2>
+              <MapContainer center={
               {lat: parseFloat(this.state.entry.location.lat) || 0,      //TODO: validate that this is between 0, 360
                 lng: parseFloat(this.state.entry.location.lng) || 0}     //TODO: validate tahtthis is between -90, 90
               } zoom={14.5} />
