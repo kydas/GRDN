75171dd6314cbe26f7ee5e837e5e426e60db1748
joshuacassidygrant <jc.grant22@gmail.com>
2019-08-05 19:19:18 -0700

    Clean up

--- constants.js
+++ constants.js
@@ -1,1 +1,5 @@
-//Ensures that imports do not fail (but contains nothing).
+export const AUTH_TOKEN = "Y2tvRjZmd1E0SXVRNlNDN3MrQzFhZz09";
+export const MAP_API_KEY = "AIzaSyBIn-I7iE0Djhfm8-u9NlFJ94Q9y2Awdho";
+export const WEATHER_KEY = "0b63023bceec4694aa8e42054dd69cd8";
+
+export default AUTH_TOKEN;
