5454b0c2dd122dda2613674bcdd798e896c329a9
joshuacassidygrant <jc.grant22@gmail.com>
2019-08-07 14:23:22 -0700

    Fixed stupid login buttons

--- imports/ui/components/LoginForm.jsx
+++ imports/ui/components/LoginForm.jsx
@@ -25,10 +25,11 @@ class LoginForm extends Component {
           <input type="text" name="username" autoComplete="username" onChange={this.handleUsernameChange} />
           <label>Password:</label>
           <input type="password" name="password" autoComplete="current-password" onChange={this.handlePasswordChange}/>
-          <div className="action-buttons">
-            <button className="purple" onClick={this.props.toggle}> Register </button>
+          <div className="action-buttons centered">
             <button className="teal" onClick={this.handleLogin}> Login </button>
-
+            <br/><br/>
+            Need an account?
+            <button className="purple" onClick={this.props.toggle}> Register </button>
           </div>
         </form>
       </div>
